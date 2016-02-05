01e4460a4a39e0123c70a3a0207024880d61de05
Thomas Morgan <thomas.j.h.morgan@gmail.com>
2016-02-05 14:53:45 -0800

    use different places for each round

--- static/js/experiment.js
+++ static/js/experiment.js
@@ -4,8 +4,7 @@ var trials_per_network;
 var trial_in_this_network = 0;
 var round_number = 0;
 var number_of_rounds;
-var all_possible_bandit_names = ["England", "Northern Ireland", "France", "Spain", "Portugal", "Germany", "Austria", "Poland", "Romania", "Ireland", "Italy", "Croatia", "Albania", "Bulgaria", "Morocco", "Libya", "Tunisia", "Egypt", "Iran", "Scotland", "Greece", "Turkey", "Sierra Leone", "Ghana", "Nigeria", "Mali", "Congo", "Ethiopia", "Chad", "Wales", "India", "Bangladesh", "Pakistan", "Afghanistan", "Australia", "New Zealand", "Tonga", "Papua New Guinea", "Fiji", "Thailand", "Laos", "Myanmar", "China", "Japan", "South Korea", "Mongolia", "Turkmenistan", "The United States", "Canada", "Greenland", "Mexico", "Guatemala", "Colombia", "Sri Lanka", "Brasil", "Argentina", "Chile", "Russia", "Ukraine", "Sweden", "Norway", "Finland", "Denmark", "Belgium", "Holland"];
-var available_bandit_names;
+var available_bandit_names = ["England", "Northern Ireland", "France", "Spain", "Portugal", "Germany", "Austria", "Poland", "Romania", "Ireland", "Italy", "Croatia", "Albania", "Bulgaria", "Morocco", "Libya", "Tunisia", "Egypt", "Iran", "Scotland", "Greece", "Turkey", "Sierra Leone", "Ghana", "Nigeria", "Mali", "Congo", "Ethiopia", "Chad", "Wales", "India", "Bangladesh", "Pakistan", "Afghanistan", "Australia", "New Zealand", "Tonga", "Papua New Guinea", "Fiji", "Thailand", "Laos", "Myanmar", "China", "Japan", "South Korea", "Mongolia", "Turkmenistan", "The United States", "Canada", "Greenland", "Mexico", "Guatemala", "Colombia", "Sri Lanka", "Brasil", "Argentina", "Chile", "Russia", "Ukraine", "Sweden", "Norway", "Finland", "Denmark", "Belgium", "Holland", "Senegal", "Botswana", "Madagascar", "Ecuador", "Costa Rica", "Nepal", "Yemen", "Gabon"];
 var bandit_names;
 
 // get all the details to correctly present the trial number bar
@@ -81,7 +80,6 @@ get_num_bandits = function() {
         success: function (resp) {
             num_bandits = resp.num_bandits;
             bandit_names = [];
-            available_bandit_names = all_possible_bandit_names;
             for (i = 0; i < num_bandits; i++) {
                 bandit_names.push("a");
             }
