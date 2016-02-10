58de963a2e4f1f873ddbac6dcfa2c30e83279b03
Thomas Morgan <thomas.j.h.morgan@gmail.com>
2016-02-10 08:42:49 -0700

    configure for 40x40

--- experiment.py
+++ experiment.py
@@ -30,8 +30,8 @@ class BanditGame(Experiment):
         self.experiment_repeats = 3
         self.practice_repeats = 0
         self.agent = BanditAgent
-        self.generation_size = 10
-        self.generations = 10
+        self.generation_size = 40
+        self.generations = 40
         self.network = lambda: BanditGenerational(generations=self.generations,
                                                   generation_size=self.generation_size,
                                                   initial_source=True)
